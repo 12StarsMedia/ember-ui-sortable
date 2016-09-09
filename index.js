d2bf5a3af344368b82b48fa42528edcea0af8f34
Nick Schot <nickschot@gmail.com>
2016-09-09 20:09:58 +0200

    Updated jquery-ui to 1.12.0 Fixed correct jquery-ui dependencies for sortable functionality

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ module.exports = {
   name: 'ember-ui-sortable',
 
   included: function(app) {
-    app.import(app.bowerDirectory + '/jquery-ui/ui/core.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/data.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/scroll-parent.js');
     app.import(app.bowerDirectory + '/jquery-ui/ui/widget.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/mouse.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/sortable.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/mouse.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/sortable.js');
   }
 };
