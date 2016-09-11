126845a08e88dae5ef355856436b88befdf68b3a
Nick Schot <nickschot@gmail.com>
2016-09-11 23:26:08 +0200

    Initial implementation of draggable component

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ module.exports = {
   name: 'ember-ui-sortable',
 
   included: function(app) {
-    app.import(app.bowerDirectory + '/jquery-ui/ui/data.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/scroll-parent.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/widget.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/mouse.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/sortable.js');
+    this._super.included(app);
+
+    //app.import(app.bowerDirectory + '/jquery-ui/ui/data.js');
+    //app.import(app.bowerDirectory + '/jquery-ui/ui/scroll-parent.js');
+    //app.import(app.bowerDirectory + '/jquery-ui/ui/widget.js');
+    //app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/mouse.js');
+    //app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/draggable.js');
+    //app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/sortable.js');
+    app.import(app.bowerDirectory + '/jquery-ui/jquery-ui.js');
   }
 };
