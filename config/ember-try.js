ce4eebb18e368d167552497f7222c81bdeb18110
Todd Smith-Salter <todd@centerdrive.ca>
2016-09-13 14:09:33 -0600

    Update to ember-cli@2.7.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,22 +1,54 @@
 /*jshint node:true*/
 module.exports = {
   scenarios: [
-    {
-      name: 'default',
-      dependencies: {
-        'ember': '1.13.13'
+      {
+        name: 'default',
+        bower: {
+          dependencies: { }
       }
     },
     {
-      name: 'ember-2.4-lts',
-      dependencies: {
-        'ember': '2.4.5'
+      name: 'ember-1.13',
+      bower: {
+        dependencies: {
+          'ember': '~1.13.0'
+        },
+        resolutions: {
+          'ember': '~1.13.0'
+        }
       }
     },
     {
-      name: 'ember-2.8-lts',
-      dependencies: {
-        'ember': '2.8.0'
+      name: 'ember-release',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#release'
+        },
+        resolutions: {
+          'ember': 'release'
+        }
+      }
+    },
+    {
+      name: 'ember-beta',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
+      }
+    },
+    {
+      name: 'ember-canary',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
       }
     }
   ]
