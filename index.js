c897c1047356ba25fdc111b7a997a34e0fc4cc04
Nick Schot <nickschot@gmail.com>
2016-09-13 14:19:05 +0200

    Update dependencies to be an exact copy of jquery-ui's custom download option

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    //app.import(app.bowerDirectory + '/jquery-ui/ui/data.js');
-    //app.import(app.bowerDirectory + '/jquery-ui/ui/scroll-parent.js');
-    //app.import(app.bowerDirectory + '/jquery-ui/ui/widget.js');
-    //app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/mouse.js');
-    //app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/draggable.js');
-    //app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/sortable.js');
-    app.import(app.bowerDirectory + '/jquery-ui/jquery-ui.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/version.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/data.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/ie.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/safe-active-element.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/safe-blur.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/scroll-parent.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/plugin.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/widget.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/mouse.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/draggable.js');
+    app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/sortable.js');
   }
 };
