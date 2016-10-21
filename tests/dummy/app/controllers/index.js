39a40841ddfbae076887897c74415768dac0563b
Todd Smith-Salter <todd@centerdrive.ca>
2016-10-21 17:30:53 -0600

    Update Readme

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -4,12 +4,16 @@ export default Ember.Controller.extend({
 
   actions: {
     contentMoved(item, oldIndex, newIndex) {
-      console.log('Content Moved! Item: ', item, "oldIndex: ", oldIndex, "newIndex: ", newIndex );
+      console.log(`Content Moved! Item: ${item}, oldIndex: ${oldIndex}, "newIndex: ${newIndex}`);
+      const content = this.get('model.sortable');
+      content.removeAt(oldIndex);
+      content.insertAt(newIndex, item);
+      console.log(`Updated content array: ${content}`);
     },
 
     addElement(item, newIndex){
-      console.log('Element added to content! Item: ', item, "newIndex: ", newIndex );
-      this.get('model.draggable').insertAt(newIndex, 'Draggable Item');
+      console.log(`Element added to content! Item: ${item.text().trim()} newIndex: ${newIndex}`);
+      this.get('model.sortable').insertAt(newIndex, item.text().trim());
     }
   }
 });
