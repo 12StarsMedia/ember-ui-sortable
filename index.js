bd8b98d881abbcd9434fe1f0a3f1d82b4466b698
Tom Panier <tpanier@synbioz.com>
2018-03-13 16:51:07 +0100

    Using node_modules instead of bower_components

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ module.exports = {
   name: 'ember-ui-sortable',
 
   included: function(app) {
-    app.import(app.bowerDirectory + '/jquery-ui/ui/version.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/data.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/ie.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/safe-active-element.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/safe-blur.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/scroll-parent.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/plugin.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/widget.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/mouse.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/draggable.js');
-    app.import(app.bowerDirectory + '/jquery-ui/ui/widgets/sortable.js');
+    app.import('node_modules/jquery-ui/ui/version.js');
+    app.import('node_modules/jquery-ui/ui/data.js');
+    app.import('node_modules/jquery-ui/ui/ie.js');
+    app.import('node_modules/jquery-ui/ui/safe-active-element.js');
+    app.import('node_modules/jquery-ui/ui/safe-blur.js');
+    app.import('node_modules/jquery-ui/ui/scroll-parent.js');
+    app.import('node_modules/jquery-ui/ui/plugin.js');
+    app.import('node_modules/jquery-ui/ui/widget.js');
+    app.import('node_modules/jquery-ui/ui/widgets/mouse.js');
+    app.import('node_modules/jquery-ui/ui/widgets/draggable.js');
+    app.import('node_modules/jquery-ui/ui/widgets/sortable.js');
   }
 };
