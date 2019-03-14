1f260eca68af49027f926d8e7d2b6b0328fa4da9
gaetan dejaegere <gdejaegere@synbioz.com>
2019-03-14 16:19:13 +0100

    feat: add fastboot support

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 /* jshint node: true */
-'use strict';
+"use strict";
 
-module.exports = {
-  name: 'ember-ui-sortable',
+const Funnel = require("broccoli-funnel");
+const Merge = require("broccoli-merge-trees");
+const fastbootTransform = require("fastboot-transform");
+const path = require("path");
+const existSync = require("exists-sync");
 
-  included: function(app) {
-    app.import('node_modules/jquery-ui/ui/version.js');
-    app.import('node_modules/jquery-ui/ui/data.js');
-    app.import('node_modules/jquery-ui/ui/ie.js');
-    app.import('node_modules/jquery-ui/ui/safe-active-element.js');
-    app.import('node_modules/jquery-ui/ui/safe-blur.js');
-    app.import('node_modules/jquery-ui/ui/scroll-parent.js');
-    app.import('node_modules/jquery-ui/ui/plugin.js');
-    app.import('node_modules/jquery-ui/ui/widget.js');
-    app.import('node_modules/jquery-ui/ui/widgets/mouse.js');
-    app.import('node_modules/jquery-ui/ui/widgets/draggable.js');
-    app.import('node_modules/jquery-ui/ui/widgets/sortable.js');
+ module.exports = {
+  name: "ember-ui-sortable",
+
+  treeForVendor(tree) {
+    let trees = [];
+
+    if (tree) {
+      trees.push(tree);
+    }
+    const assetDir = path.join(this.project.root, "node_modules", "jquery-ui");
+
+    if (existSync(assetDir)) {
+      const browserTrees = fastbootTransform(new Funnel(assetDir, {
+        srcDir: "ui",
+        destDir: "jquery-ui/ui"
+      }));
+      trees.push(browserTrees);
+    }
+    return new Merge(trees);
+  },
+
+  included(app) {
+    this._super.included(app);
+    app.import("vendor/jquery-ui/ui/version.js");
+    app.import("vendor/jquery-ui/ui/data.js");
+    app.import("vendor/jquery-ui/ui/ie.js");
+    app.import("vendor/jquery-ui/ui/safe-active-element.js");
+    app.import("vendor/jquery-ui/ui/safe-blur.js");
+    app.import("vendor/jquery-ui/ui/scroll-parent.js");
+    app.import("vendor/jquery-ui/ui/plugin.js");
+    app.import("vendor/jquery-ui/ui/widget.js");
+    app.import("vendor/jquery-ui/ui/widgets/mouse.js");
+    app.import("vendor/jquery-ui/ui/widgets/draggable.js");
+    app.import("vendor/jquery-ui/ui/widgets/sortable.js");
   }
 };
